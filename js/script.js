c919747efc6ce7fc90791c06e286cd4138d7725c
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-01 16:18:20 +0100

    taken discs from api

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: '#root',
     data: {
-
+        disc: [],
+    },
+    methods: {
+        searchDisc() {
+            axios.get('http://localhost/pomeriggio/php-ajax-dischi/includes/var.php').then(
+                res => { this.disc = res.data; }
+            ).catch(err => { console.log(err) });
+        }
+    },
+    mounted() {
+        this.searchDisc();
     }
 })
