7b20291fc797aabdf53908d7dd538289938c7cbf
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-01 15:52:19 +0100

    created includes

---
+++ includes/card.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <div class="card-image">
+        <figure>
+            <img src="<?= $disc['poster'] ?>" alt="a">
+        </figure>
+    </div>
+    <div class="card-title">
+        <h2><?= strtoupper($disc['title'])  ?></h2>
+    </div>
+    <div class="card-author">
+        <?= $disc['author'] ?>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,50 +4,7 @@ Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i disch
 
 <?php
 
-$database = [
-    [
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
-        'year' => 1988,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
-        'genre' => 'Rock'
-    ],
-    [
-        'title' => 'Live at Wembley 86',
-        'author' => 'Queen',
-        'year' => 1992,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-        'genre' => 'Pop'
-    ],
-    [
-        'title' => 'Ten\'s Summoner\'s Tales',
-        'author' => 'Sting',
-        'year' => 1993,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-        'genre' => 'Pop'
-    ],
-    [
-        'title' => 'Steve Gadd band',
-        'author' => 'Steve Gadd Band',
-        'year' => 2018,
-        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
-        'genre' => 'Jazz'
-    ],
-    [
-        'title' => 'Brave new World',
-        'author' => 'Iron Maiden',
-        'year' => 2000,
-        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
-        'genre' => 'Metal'
-    ],
-    [
-        'title' => 'One more car, one more rider',
-        'author' => 'Eric Clapton',
-        'year' => 2002,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
-        'genre' => 'Rock'
-    ]
-];
+include 'includes/var.php';
 
 ?>
 
@@ -70,19 +27,7 @@ $database = [
         <div class="container">
             <!-- ciclo per prendre ogni disco -->
             <?php foreach ($database as $disc) : ?>
-                <div class="card">
-                    <div class="card-image">
-                        <figure>
-                            <img src="<?= $disc['poster'] ?>" alt="a">
-                        </figure>
-                    </div>
-                    <div class="card-title">
-                        <h2><?= strtoupper($disc['title'])  ?></h2>
-                    </div>
-                    <div class="card-author">
-                        <?= $disc['author'] ?>
-                    </div>
-                </div>
+                <?php include 'includes/card.php' ?>
             <?php endforeach ?>
         </div>
     </main>
